d9b2591424aeb868a88e0d56bd5e1ca161377bb0
Eric Oswald <eoswald39@gmail.com>
2014-01-25 16:16:05 -0500

    Added LED with PWM to test

--- photocell_test/photocell_test.ino
+++ photocell_test/photocell_test.ino
@@ -1,9 +1,13 @@
 #define SENSOR A0
+#define LED 9
+#define PW_LED 10
 
 void setup() {
+  pinMode(LED, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
+  analogWrite(LED, PW_LED);
   Serial.println(analogRead(SENSOR));
 }
